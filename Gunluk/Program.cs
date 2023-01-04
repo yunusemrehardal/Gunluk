42a5b478a44b46fb60e6a6eac4856777b0321e4b
yunusemrehardal <hardaalyunusemre@gmail.com>
2023-01-04 15:26:33 +0300

    Data

--- Gunluk/Program.cs
+++ Gunluk/Program.cs
@@ -1,6 +1,10 @@
+global using Gunluk.Data;
+global using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<UygulamaDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("UygulamaDbContext")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
